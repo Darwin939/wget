4c8740da5707a6e90ec97ce856cd0ad702761d68
Dawrld <Dawrld>
2022-07-30 18:46:17 +0600

    fix sentense

--- internal/service/presenter/presenter.go
+++ internal/service/presenter/presenter.go
@@ -25,7 +25,7 @@ func (c *CLIPresenter) ShowRequestStatus(statusCode int) {
 	if statusCode >= 200 && statusCode <= 299 {
 		fmt.Printf("sending request, awaiting response... status %v OK\n", statusCode)
 	} else {
-		fmt.Printf("sending request, awaiting response... status %v \n", statusCode, "something goes wrong")
+		fmt.Printf("sending request, awaiting response... status %v something goes wrong\n", statusCode)
 	}
 
 }
